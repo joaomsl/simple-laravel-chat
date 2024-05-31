11e4fed5a99d9f95015320122176bba3d6fc1500
João M <109992990+joaomsl@users.noreply.github.com>
2024-05-31 17:49:56 -0300

    routes for chat

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\ChatPage;
+use App\Livewire\JoinPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ChatPage::class)
+    ->middleware('auth')
+    ->name('chat');
+
+Route::get('/join', JoinPage::class)
+    ->middleware('guest') 
+    ->name('join');
