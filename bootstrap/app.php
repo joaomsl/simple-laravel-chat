c8b828ce6bad731d1868d6452c719b2fef21ac54
João M <109992990+joaomsl@users.noreply.github.com>
2024-05-31 17:49:45 -0300

    disable health route and fix auth routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,10 +8,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn() => route('join'))
+            ->redirectUsersTo(fn() => route('chat'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
